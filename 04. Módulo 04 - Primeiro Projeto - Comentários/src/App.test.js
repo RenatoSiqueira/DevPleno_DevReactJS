a8b0178103aa490df514133655d28c9ecd3ebbf9
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-01-03 00:19:22 -0300

    M10 - Projeto - Aula 10

--- 04. Módulo 04 - Primeiro Projeto - Comentários/src/App.test.js
+++ 04. Módulo 04 - Primeiro Projeto - Comentários/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import App from './App'
 import { shallow } from 'enzyme'
+import { EventEmitter } from 'events'
 
 import Comments from './Comments'
 import NewComment from './NewComment'
@@ -43,5 +44,40 @@ describe('<App />', () => {
     expect(update).toBeCalledWith({
       'comments/1': { comment: 'new comment' }
     })
+  })
+
+  it('renders comments from firebase', () => {
+    const database = {
+      ref: jest.fn()
+    }
+    const eventEmitter = new EventEmitter()
+    database.ref.mockReturnValue(eventEmitter)
+
+    const wrapper = shallow(<App database={database} />)
+
+    // Não Recebeu Comments
+    expect(wrapper.find(Comments).length).toBe(1)
+    expect(wrapper.find(NewComment).length).toBe(1)
+    expect(wrapper.find('p').length).toBe(1)
+
+    // Recebeu Comments
+    const comments = {
+      a: { comment: 'Comment 1' },
+      b: { comment: 'Comment 2' }
+    }
+    const val = jest.fn()
+    val.mockReturnValue(comments)
+    eventEmitter.emit('value',  {
+      val
+    })
+
+    wrapper.update()
+
+    // Tests
+    expect(wrapper.state().isLoading).toBeFalsy()
+    expect(wrapper.state().comments).toBe(comments)
+    expect(wrapper.find(Comments).get(0).props.comments).toBe(comments)
+    expect(wrapper.find(NewComment).get(0).props.sendComment).toBe(wrapper.instance().sendComment)
+    expect(wrapper.find('p').length).toBe(0)
   })
 })
