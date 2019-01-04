4cc68f69f586e69e9c3447cbbf1d90ba815deb42
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-01-04 17:01:14 -0300

    05. Módulo 05 - Aula 03

--- 05. Módulo 05 - Primeiro Projeto React Native/MeuIP/App.js
+++ 05. Módulo 05 - Primeiro Projeto React Native/MeuIP/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
+import logo from './assets/logo.png'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: ''
+    }
+
+    this.findMyIp = this.findMyIp.bind(this)
+  }
+  async findMyIp(){
+    this.setState({ data: 'Descobrindo Ip..' })
+    const ip = await fetch('http://httpbin.org/ip')
+    const data = await ip.json()
+    this.setState({ data: data.origin })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.body}>
+          <Image source={logo} />
+          <Text style={styles.ip}>{this.state.data}</Text>
+          <Button title='Descobrir meu IP' onPress={ this.findMyIp }/>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.made}>
+            Feito com Amor
+          </Text>
+        </View>
       </View>
     );
   }
@@ -14,8 +40,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#2F2336',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  ip: {
+    color: 'white',
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  made: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  footer: {
+    paddingTop: 10,
+    paddingBottom: 10
+  }
 });
