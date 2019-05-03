4cd4519d2264d4d7e23ac08cdc9854102c227de6
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-05-03 00:11:48 -0300

    M07

--- 07. Módulo 07 - Projeto 2 - Gerenciador de Produtos/src/Categoria.js
+++ 07. Módulo 07 - Projeto 2 - Gerenciador de Produtos/src/Categoria.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Categoria extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            produtos: [],
+            categoria: []
+        }
+        this.loadData = this.loadData.bind(this)
+    }
+    loadData(id){
+        axios
+        .get('http://localhost:3001/produtos?categoria=' + id)
+        .then( res => this.setState({ produtos: res.data }))
+
+        axios
+        .get('http://localhost:3001/categorias/' + id)
+        .then( res => this.setState({ categoria: res.data }))
+    }
+    componentDidMount() {
+        const id = this.props.match.params.catId
+        this.loadData(id)
+    }
+    componentWillReceiveProps(newProps) {
+        this.loadData(newProps.match.params.catId)
+    }
+    renderProduto(produto){
+        return (
+            <div class='card' key={produto.id}>
+                <div class='card-body'>
+                    <p>{produto.produto}</p>
+                </div>
+            </div>
+        )
+    }
     render() {
-        return <h1>Categoria {this.props.match.params.catId}</h1>
+        return (
+            <div>
+                <h1>{this.state.categoria.categoria}</h1>
+                {
+                    this.state.produtos.map(this.renderProduto)
+                }
+            </div>
+        )
     }
 }
 
